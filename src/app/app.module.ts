c465f86c5bfad5ff7c3848b08aeac67909d2ca13
Mauricio Duarte <prussdev@gmail.com>
2019-10-26 14:40:18 -0300

    Projeto criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { VeiculosComponent } from './veiculos/veiculos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VeiculosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
